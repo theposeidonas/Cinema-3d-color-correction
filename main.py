a1ca954fe22b42d282d3f20816af54d638ba0ab3
theposeidonas <baran9arda@gmail.com>
2018-12-20 16:56:10 +0300

    Main code
    Code to Cinema 3D will execute

---
+++ main.py
@@ -0,0 +1,33 @@
+
+def main():
+    if giris1 is '0':
+     renkismi = ''
+     r = 255
+     g = 255
+     b = 255
+    if giris1 == '1':
+     renkismi = 'CD-8000'
+     r = 236
+     g = 197
+     b = 190
+    if giris1 == '2':
+     renkismi = 'CD-8001'
+     r = 236
+     g = 197
+     b = 190
+    if giris1 == '3':
+     renkismi = 'CD-8002'
+     r = 221
+     g = 157
+     b = 174
+
+    if giris1 == '4':
+     renkismi = 'CD-8003'
+     r = 199
+     g = 101
+     b = 123
+    name=renkismi
+    r_real=r
+    b_real=b
+    g_real=g
+
